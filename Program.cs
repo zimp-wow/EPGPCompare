ccd00c156c828ec1ad514b63c80ec03a627ce83a
Zimp <zimp@zenryo.xyz>
2020-09-12 02:38:01 -0500

    Initial commit.

---
+++ Program.cs
@@ -0,0 +1,171 @@
+﻿using EPGPAnalyze;
+using System;
+using System.Diagnostics;
+
+namespace EPGPCompare
+{
+	class Program
+	{
+		const bool DEBUG_LOGS = true;
+
+		static void Main(string[] args)
+		{
+			Console.WriteLine( "Choose Mode: " );
+			Console.WriteLine( "    1. Overall" );
+			Console.WriteLine( "    2. Simulate" );
+			Console.WriteLine();
+			Console.Write( "? " );
+
+			if( int.TryParse( Console.ReadLine(), out int choice ) ) {
+				switch( choice ) {
+					case 1:
+						OverallStandings();
+						break;
+					case 2:
+						Analyze();
+						break;
+				}
+			}
+		}
+
+		static void OverallStandings()
+		{
+			Standings standings = new Standings();
+
+			standings.ProcessFile( "standings.txt" ).Wait();
+
+			Console.Write( "Your name: " );
+			string playername = Console.ReadLine().Trim();
+
+			Standings.Entry yourEntry = standings.Entries.Find( ( entry ) => entry.Name.ToLower() == playername.ToLower() );
+			if( yourEntry == null ) {
+				Console.WriteLine( "Couldnt find player" );
+				return;
+			}
+
+			Console.WriteLine( $"You: { yourEntry.Name } -- { yourEntry.EP }/{ yourEntry.GP } -- { yourEntry.PR }" );
+			Console.WriteLine();
+
+			foreach( Standings.Entry otherEntry in standings.Entries ) {
+				if( otherEntry.Name == yourEntry.Name ) {
+					continue;
+				}
+
+				if( otherEntry.PR == 0 ) {
+					continue;
+				}
+
+				if( otherEntry.PR > yourEntry.PR ) {
+					Console.WriteLine( "You lose to '" + otherEntry.Name + "' because your PR is lower" );
+					continue;
+				}
+
+				if( otherEntry.PR == yourEntry.PR ) {
+					Console.WriteLine( "You tie with '" + otherEntry.Name + "' because your PR is the same" );
+					continue;
+				}
+
+				double PRDiff = yourEntry.PR - otherEntry.PR;
+				double allowance = yourEntry.EP / otherEntry.PR - yourEntry.GP;
+
+				Console.WriteLine( $"If you spend {allowance:F2} GP you're PR will match { otherEntry.Name }'s ({ otherEntry.EP }/{ otherEntry.GP } -- { otherEntry.PR })" );
+			}
+		}
+
+		static void Analyze() {
+			Standings standings = new Standings();
+
+			standings.ProcessFile( "standings.txt" ).Wait();
+
+			Console.Write( "Your name: " );
+			string playername = Console.ReadLine().Trim();
+
+			Standings.Entry yourEntry = standings.Entries.Find( ( entry ) => entry.Name.ToLower() == playername.ToLower() );
+			if( yourEntry == null ) {
+				Console.WriteLine( "Couldnt find player" );
+				return;
+			}
+
+			Console.WriteLine( $"{ yourEntry.Name } -- { yourEntry.EP }/{ yourEntry.GP } -- { yourEntry.PR }" );
+			Console.WriteLine();
+
+			Console.Write( "Competitor: " );
+			string competitor = Console.ReadLine().Trim();
+
+			Standings.Entry otherEntry = standings.Entries.Find( ( entry ) => entry.Name.ToLower() == competitor.ToLower() );
+			if( otherEntry == null ) {
+				Console.WriteLine( "Couldnt find player" );
+				return;
+			}
+			
+			Console.WriteLine( $"{ otherEntry.Name } -- { otherEntry.EP }/{ otherEntry.GP } -- { otherEntry.PR }" );
+			Console.WriteLine();
+
+			Console.Write( "Points you want to spend: " );
+			int points = 0;
+			if( !int.TryParse( Console.ReadLine(), out points ) ) {
+				Console.WriteLine( "Invalid points" );
+				return;
+			}
+
+			yourEntry.GP += points;
+			yourEntry.PR = double.Parse( $"{((double)yourEntry.EP / yourEntry.GP):F2}" );
+
+			Console.WriteLine( $"{ yourEntry.Name } -- { yourEntry.EP }/{ yourEntry.GP } -- { yourEntry.PR }" );
+			Console.WriteLine();
+
+			if( yourEntry.PR <= otherEntry.PR ) {
+				Console.WriteLine( "You immediately lose priority to your competitor after this award" );
+				return;
+			}
+
+			int decay( int val, double percent, int baseVal = 0 ) {
+				if( baseVal > 0 ) {
+					return (int)Math.Max( Math.Floor( ( val - baseVal ) * ( 1.0 - percent ) + baseVal ), baseVal );
+				}
+
+				return (int)Math.Max( Math.Floor( val * ( 1.0 - percent ) ), 0 );
+			}
+
+			int iterations = 0;
+			int baseGP = 150;
+			int weeklyEP = 300;
+			while( true ) {
+				iterations++;
+
+				yourEntry.EP += weeklyEP;
+				otherEntry.EP += weeklyEP;
+
+				yourEntry.EP = decay( yourEntry.EP, 0.1 );
+				yourEntry.GP = decay( yourEntry.GP, 0.1, baseGP );
+				yourEntry.PR = double.Parse( $"{((double)yourEntry.EP / yourEntry.GP):F2}" );
+
+				otherEntry.EP = decay( otherEntry.EP, 0.1 );
+				otherEntry.GP = decay( otherEntry.GP, 0.1, baseGP );
+				otherEntry.PR = double.Parse( $"{((double)otherEntry.EP / otherEntry.GP):F2}" );
+
+				if( DEBUG_LOGS ) {
+					Console.WriteLine( "Iteration: " + iterations );
+					Console.WriteLine( $"{ yourEntry.Name } -- { yourEntry.EP }/{ yourEntry.GP } -- { yourEntry.PR }" );
+					Console.WriteLine( $"{ otherEntry.Name } -- { otherEntry.EP }/{ otherEntry.GP } -- { otherEntry.PR }" );
+					Console.WriteLine();
+				}
+
+				if( otherEntry.PR >= yourEntry.PR ) {
+					break;
+				}
+				
+				if( otherEntry.PR == yourEntry.PR ) {
+					break;
+				}
+			}
+
+			Console.WriteLine( $"It took { iterations } weeks for { otherEntry.Name } to catch you in PR after you spent GP." );
+			Console.WriteLine( "Assuming you both had identical raid attendance" );
+
+			Console.WriteLine();
+			Console.WriteLine( $"{ yourEntry.Name } -- { yourEntry.EP }/{ yourEntry.GP } -- { yourEntry.PR }" );
+			Console.WriteLine( $"{ otherEntry.Name } -- { otherEntry.EP }/{ otherEntry.GP } -- { otherEntry.PR }" );
+		}
+	}
+}
---
+++ Standings.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace EPGPAnalyze
+{
+	class Standings
+	{
+		public List<Entry> Entries { get; set; } = new List<Entry>();
+
+		public Standings() {
+		}
+
+		public async Task ProcessFile( string file ) {
+			
+			using( StreamReader sr = new StreamReader( file ) ) {
+				while( !sr.EndOfStream ) {
+					string line  = await sr.ReadLineAsync();
+
+					try {
+						Entry entry = new Entry( line );
+
+						Entries.Add( entry );
+					}
+					catch( Exception e ) {
+						Console.WriteLine( "Failed to parse line: " + line + " - " + e );
+					}
+				}
+			}
+
+			Entries.Sort( ( left, right ) => { return right.PR.CompareTo( left.PR ); } );
+		}
+
+		public class Entry {
+			public string   Name    { get; set; }
+			public string   Class   { get; set; }
+			public string   Role    { get; set; }
+			public int      EP      { get; set; }
+			public int      GP      { get; set; }
+			public double   PR      { get; set; }
+
+			public Entry( string line ) {
+
+				string[] comps = line.Split( ',' );
+				if( line.Length < 3 ) {
+					throw new Exception( "Unexpected number of fields" );
+				}
+
+				Name  = string.Empty;
+				// Stripping any non-ascii characters from the name, thanks Auslander
+				foreach( char c in comps[0].ToCharArray() ) {
+					if( c <= sbyte.MaxValue ) {
+						Name += c;
+					}
+				}
+
+				Class = comps[1];
+				Role  = comps[2];
+				EP = 0;
+				GP = 0;
+				PR = 0.0f;
+
+				if( comps.Length < 4 ) {
+					return;
+				}
+
+				if( string.IsNullOrWhiteSpace( comps[3] ) ) {
+					comps[3] = "0";
+				}
+
+				EP = int.Parse( comps[3] );
+
+				if( comps.Length < 5 ) {
+					return;
+				}
+
+				if( string.IsNullOrWhiteSpace( comps[4] ) ) {
+					comps[4] = $"{ 0 }";
+				}
+
+				GP = int.Parse( comps[4] );
+
+				if( comps.Length < 6 ) {
+					return;
+				}
+
+				if( string.IsNullOrWhiteSpace( comps[5] ) ) {
+					comps[5] = "0";
+				}
+
+				PR = double.Parse( comps[5] );
+			}
+		}
+	}
+}
